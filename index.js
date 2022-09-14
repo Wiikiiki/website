483ce9991773c811a62097368b9b85e513a060bc
Wiikiiki <wiikiiki@outlook.com>
2022-09-14 22:34:37 +0800

    smooth scroll & data (dynamic)

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ window.addEventListener("scroll", () => {
   }
 })
 
+
 // 轮播图
 const glide = new Glide(".glide")
 const captionsEl = document.querySelectorAll('.slide-caption')
@@ -43,6 +44,7 @@ glide.on("run.before",() => {
 
 glide.mount( )
 
+
 // 筛选
 const isotope = new Isotope(".cases", {
   layoutMode: "fitRows",
@@ -60,3 +62,58 @@ filterBtns.addEventListener("click", e => {
     isotope.arrange({filter: filterOption})
   }
 })
+
+
+// 浮动出现
+const staggeringOption = {
+  delay: 300,
+  distance: "50px",
+  duration: 500,
+  easing: "ease-in-out",
+  origin: "bottom"
+}
+
+ScrollReveal().reveal(".feature", {...staggeringOption, interval: 350})
+ScrollReveal().reveal(".service-item", {...staggeringOption, interval: 350})
+
+const dataSectionEl = document.querySelector(".data-section")
+ScrollReveal().reveal(".data-section", {
+  beforeReveal:() => {
+    anime({
+      targets: ".data-piece .num",
+      innerHTML: el => {
+        return [0, el.innerHTML]
+      },
+      duration: 2000,
+      round: 1,
+      easing: "easeInExpo"
+    })
+    dataSectionEl.style.backgroundPosition = `center calc(50% - ${dataSectionEl.getBoundingClientRect().bottom / 5 }px)`
+  }
+})
+
+
+// 数据背景视差
+window.addEventListener("scroll", () => {
+  const bottom = dataSectionEl.getBoundingClientRect().bottom
+  const top = dataSectionEl.getBoundingClientRect().top
+
+  if (bottom >= 0 && top <= window.innerHeight) {
+    dataSectionEl.style.backgroundPosition = `center calc(50% - ${bottom/5}px)`
+  }
+})
+
+
+// 导航滑动跳转
+const scroll = new SmoothScroll('nav a[href*="#"], .scrollToTop a[href*="#"]', {
+  header: "header",
+  offset: 80
+})
+
+const expoloreBtnEls = document.querySelectorAll(".explore-btn")
+
+expoloreBtnEls.forEach( expoloreBtnEl => { 
+  expoloreBtnEl.addEventListener("click", () => {
+    scroll.animateScroll(document.querySelector("#about-us"))
+  })
+});
